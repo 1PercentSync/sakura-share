b69180ecc83c22d7a4980b63f642bb2ced29b245
1PercentSync <127626920+1PercentSync@users.noreply.github.com>
2024-10-13 19:07:41 -0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -8,54 +8,16 @@ export default {
 
         const method = request.method;
 
-        // Handle preflight (OPTIONS) requests
-        if (method === 'OPTIONS') {
-            return new Response(null, {
-                status: 204,
-                headers: {
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-                    'Access-Control-Allow-Headers': 'Authorization, Content-Type',
-                    'Access-Control-Max-Age': '86400',  // Cache preflight response for 1 day
-                }
-            });
-        }
-
         if (pathname === '/register-node') {
             return await registerNode(request, env);
         } else if (pathname === '/verify-node') {
             return await verifyNode(request, env);
         } else if (pathname === '/delete-node') {
             return await deleteNode(request, env);
-        } else if (pathname === '/completion' || pathname === '/completions' || pathname === '/v1/chat/completions') {
+        } else if (pathname === '/completion' || pathname === '/completions' || pathname === '/v1/chat/completions' || pathname === '/v1/models') {
             return await handleOtherRequests(request, env);
         } else if (pathname === '/health') {
             return await getHealthStatus(env);
-        } else if (pathname === '/v1/models') {
-            return new Response(JSON.stringify({
-                "object": "list",
-                "data": [
-                    {
-                        "id": "sakura-14b-qwen2.5-v1.0-iq4xs",
-                        "object": "model",
-                        "created": 1728830939,
-                        "owned_by": "llamacpp",
-                        "meta": {
-                            "vocab_type": 2,
-                            "n_vocab": 152064,
-                            "n_ctx_train": 131072,
-                            "n_embd": 5120,
-                            "n_params": 14770033664,
-                            "size": 8180228096
-                        }
-                    }
-                ]
-            }), {
-                status: 200,
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
         }
         return new Response('Not found', { status: 404 });
     }
